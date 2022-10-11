bb2725c5690854330a28b958ccae733fe49ef35e
Vitor Hugo Cassol <vhcassol@gmail.com>
2022-10-11 12:30:18 -0400

    Feature: Consumo da API para iniciar a aplicação

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,47 +1,44 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    {{queryAtual}}
+  </div>
 </template>
 
+<script>
+
+
+export default {
+  name: 'App',
+  
+  data() {
+    return {
+      queryAtual: undefined,
+      queryProxima: undefined,
+      queryAnterior: undefined
+    }
+  },
+  
+  methods: {
+    getPokemonPage(query) {
+      this.queryAtual = query;
+
+      this.axios
+      .get(query)
+      .then((response) => {
+        this.queryProxima = response.data.next;
+        this.queryAnterior = response.data.previous;
+      })
+    }
+  },
+
+  created() {
+    this.getPokemonPage("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0")
+  }
+
+}
+
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
